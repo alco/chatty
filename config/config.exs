03dca8d2d73e660bc3547a2c5d9096f742887a57
Alexei Sholik <alcosholik@gmail.com>
2015-10-05 10:46:03 +0300

    Set default logging level to :debug

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,18 @@
 use Mix.Config
 
-config :chatty,
+config :chatty, [
   host: "irc.freenode.net",
   port: 6667,
   nickname: "testbotunique",
-  channels: ["test-secret-channel"]
+  channels: ["test-secret-channel"],
+]
 
 # Use a custom format with a single line break at the end
-config :logger, :console,
+config :logger, :console, [
   format: "$time $metadata[$level] $levelpad$message\n",
-  level: :info
+  level: :debug,
+]
+
+config :logger, [
+  handle_otp_reports: true,
+]
